137b59b1540fa4c7e05d15c4adb6d00596fa166a
Rimuruu <antoine.renciot@gmail.com>
2022-03-23 16:47:48 +0100

    tp4

--- compilo/Generate.ml
+++ compilo/Generate.ml
@@ -14,77 +14,92 @@ let val_to_reg = fun i ->
   | 5 -> "%r9"
   | _ -> "null"
 
+let taille_expr = fun e -> 
+  let rec pop_e acc exp  = 
+    match exp with
+    | IntegerLiteral i -> 1 + acc
+    | BOp (b,bo,b2) -> 1 + acc + (pop_e 0 b) + (pop_e 0 b2)
+    | UOp (uop,b1) -> 1 + acc + (pop_e 0 b1)
+    | Var v -> 1 +acc
+    | Set (v,e1) -> pop_e (acc+1) e1 
+    | Call(fn,e1) -> List.fold_left pop_e acc e1 
+    | StringLiteral s -> 1+acc
+  in
+  pop_e 0 e
+
+  let taille_stat = fun stat -> 
+    let rec pop_stat acc st = 
+    match st with
+    | CompoundStmt( vl, ls) -> (List.length vl) + (List.fold_left pop_stat acc ls )
+    | IfStmt (e,s1,s2) -> (taille_expr e) + acc + (pop_stat 0 s1) + (pop_stat 0 s2)
+    |Expr e -> (taille_expr e) + acc
+    |WhileStmt(e,s) -> pop_stat acc s                           
+    | ReturnStmt None -> acc
+    | ReturnStmt (Some e) -> acc
+    in
+  pop_stat 0 stat
+
+  let taille_decl = fun decl -> 
+  let rec pop_decl acc dcl = 
+  match dcl with                      
+  | FunctionDecl(fn,al,s) -> taille_stat s           
+  | VarDecl(_) -> 0
+  in
+  pop_decl 0 decl
+
+
+let check_inline = fun s  -> 
+    let f = (getFunDec s) in
+    if (taille_decl f) < 40  then true else false
+
+(*let register = ["%rdi";"%rsi";"%rdx";"%rcx";"%r8";"%r9"]*)
+
 let generate_asm_un_op = fun varl sp uop il ->
   match uop with
-  | Not -> (il  |+ "xorq $1, %rax" )
+  | Not -> (il  |+ "cmpq $0, %rax" |+ "sete %al" )
   | MinusM -> (il  |+ "negq %rax" )
 
 let generate_asm_bin_op2 = fun varl sp bop il ->
     match bop with
-    |Mult -> (il |+ "imulq (%rsp),%rax"|+ "addq $8 , %rsp")
-    |Add -> (il |+ "addq (%rsp),%rax"|+ "addq $8 , %rsp")
-    |Sub -> (il |+ "negq %rax" |+ "addq (%rsp),%rax" |+ "addq $8 , %rsp")
-    |EQ -> (il  |+ "cmpq (%rsp),%rax" |+ "sete %al" |+ "addq $8 , %rsp")
-    |NEQ -> (il  |+ "cmpq (%rsp),%rax" |+ "setne %al" |+ "addq $8 , %rsp")
-    |LE -> (il  |+ "cmpq (%rsp),%rax" |+ "setns %al" |+ "addq $8 , %rsp")
-    |LL -> (il  |+ "cmpq (%rsp),%rax" |+ "setg %al" |+ "addq $8 , %rsp")
-    |Div -> (il  |+ "movq %rax, %rdx" |+ "movq (%rsp), %rax" |+ "movq %rdx, (%rsp)" |+ "idivq (%rsp) ,%rax " |+ "addq $8 , %rsp")
-    |Mod -> (il |+ "movq %rax, %rdx" |+ "movq (%rsp), %rax" |+ "movq %rdx, (%rsp)" |+ "movq $0,%rdx" |+ "idivq (%rsp),%rax" |+ "movq %rdx, %rax" |+ "addq $8 , %rsp")
-    |Or -> (il  |+ "orq (%rsp),%rax" |+ "addq $8 , %rsp")
-    |And -> (il  |+ "andq (%rsp),%rax" |+ "addq $8 , %rsp")
-(*let rec generate_asm_bin_op = fun varl sp bop il ->
-    try match bop with
-    | BOp (IntegerLiteral i,bo,IntegerLiteral i2) -> let ill = il |+ "movq $"^string_of_int i^", %rax"
-                                                                      |+  "pushq %rax" 
-                                                                      |+ "movq $"^string_of_int i2^", %rax"in
-                    
-                                                          generate_asm_bin_op2 varl sp bo ill
-    | BOp (IntegerLiteral i,bo,b) -> let ill = il |+ "movq $"^string_of_int i^", %rax"
-                                           |+  "pushq %rax" in
-                                           let ill2 = generate_asm_bin_op varl sp b ill in
-                                           generate_asm_bin_op2 varl sp bo ill2
-    | BOp (b,bo,IntegerLiteral i) -> let ill = (generate_asm_bin_op varl sp b il)|+"pushq %rax" in
-                                     let ill2 = ill |+ "movq $"^string_of_int i^", %rax" in
-                                              generate_asm_bin_op2 varl sp bo ill2
-    
-    with Match_failure(_) -> raise (Code_gen_failure_expression bop)
-  *)
+    |Mult -> (il |+ "imulq (%rsp),%rax")
+    |Add -> (il |+ "addq (%rsp),%rax")
+    |Sub -> (il |+ "subq (%rsp),%rax" )
+    |EQ -> (il  |+ "movq %rax, %rbx" |+ "xorq %rax, %rax" |+ "cmpq %rbx,(%rsp)" |+ "sete %al")
+    |NEQ -> (il  |+ "movq %rax, %rbx" |+ "xorq %rax, %rax" |+ "cmpq %rbx,(%rsp)" |+ "setne %al" )
+    |LE -> (il  |+ "movq %rax, %rbx" |+ "xorq %rax, %rax" |+ "cmpq %rbx,(%rsp)" |+ "setns %al" )
+    |LL -> (il  |+ "movq %rax, %rbx" |+ "xorq %rax, %rax" |+ "cmpq %rbx,(%rsp)" |+ "setg %al" )
+    |Div -> (il  |+ "xorq %rdx,%rdx" |+ "idivq (%rsp) ,%rax " )
+    |Mod -> (il |+ "xorq %rdx,%rdx" |+ "idivq (%rsp),%rax" |+ "movq %rdx, %rax" )
+    |Or -> (il  |+ "orq (%rsp),%rax" )
+    |And -> (il  |+ "andq (%rsp),%rax" )
 
 
 
 let rec generate_asm_expression = fun varl sp e il ->
-  let rec mov_reg = fun varl sp el il i sp2 -> 
+  let rec call_stack = fun varl sp el il i sp2 -> 
     match el with 
     | [] -> (il,sp2)
     | e::s -> 
-              let sp3 = sp2+8 in 
-              let (il1,sp4) = (mov_reg varl sp s il (i+1) (sp3)) in
+              let sp3 = sp+8 in 
+              let (il1,sp4) = (call_stack varl sp s il (i+1) (sp3)) in
               let il2 = generate_asm_expression varl sp e il1 in 
-              let il4 = il2 |+ "movq %rax, "^(val_to_reg i) in
+              let il4 = il2 |+ "pushq %rax" in
               (il4,sp4)
     in
-  let rec save_reg = fun i il->
-    if i < 6 then let il2 = il |+ "pushq "^(val_to_reg i) in (save_reg (i+1) il2)
-    else il
-  in
-  let rec load_reg = fun i il->
-    if i >= 0 then let il2 = il |+ "popq "^(val_to_reg i) in (load_reg (i-1) il2)
-    else il
-  in
-  let rec rsp_mod = fun sp i il ->
-      if (sp mod 16) == 0 then (il,i)
-      else let il2 = il |+ "pushq %rax" in (rsp_mod (sp+8) (i+1) il2) 
-  in
-  let rec rsp_unmod = fun i il ->
-      if i < 0 then il
-      else let il2 = il |+ "popq %rdx" in (rsp_unmod (i-1) il2) 
-  in
+  let rec pop_reg = fun varl sp el il i sp2 -> 
+    match el with 
+    | [] -> (il,sp2)
+    | e::s -> 
+              let il2 = il |+ "popq "^(val_to_reg i) in
+              let (il3,sp4) = (pop_reg varl sp s il2 (i+1) (sp-8)) in  
+              (il3,sp4)
+    in
   try match e with
       (* *)
   | IntegerLiteral i -> (il |+ "movq $"^string_of_int i^", %rax" )
-  | BOp (b,bo,b2) -> let ill = (generate_asm_expression varl sp b il)|+"pushq %rax" in
-                     let ill2 = (generate_asm_expression varl sp b2 ill ) in
-                     (generate_asm_bin_op2  varl sp bo ill2 )
+  | BOp (b,bo,b2) -> let ill = (generate_asm_expression varl sp b2 il)|+"pushq %rax" in
+                     let ill2 = (generate_asm_expression varl sp b ill ) in
+                     (generate_asm_bin_op2  varl sp bo ill2 ) |+ "addq $8 , %rsp"
   | UOp (uop,b1) -> let ill = (generate_asm_expression varl sp b1 il)in
                      (generate_asm_un_op varl sp uop ill )
   | Var v -> let b = List.assoc v varl in
@@ -92,10 +107,15 @@ let rec generate_asm_expression = fun varl sp e il ->
   |Set (v,e) ->  let il2 = (generate_asm_expression varl sp e il) in
                  let addr = (List.assoc v varl) in
                 (il2 |+ "movq %rax, "^addr )
-  | Call(fn,exp) -> let ill = save_reg 0 il in
-                    let (il2,sp2) = (mov_reg varl sp exp ill 0 0) in
-                   let il3 = (( il2 |+ ("callq "^fn))) in
-                  (load_reg 5 il3)       
+  | Call(fn,exp) -> let ill,sp2 = call_stack varl sp exp il 0 0 in
+                    let ill2,sp3 = pop_reg varl sp2 exp ill 0 0 in
+                    if( sp3 mod 16 <> 0) then 
+                            let il3 = ill2 |+ "subq $8, %rsp" in
+                            let il4 = (( il3 |+ ("callq "^fn))) in
+                            il4 |+ "addq $8, %rsp"  
+                    else 
+                            let il3 = (( ill2 |+ ("callq "^fn))) in
+                            il3
   | StringLiteral s -> let addr = addr_lbl_of_string s in
                         (il |+ "leaq "^addr^", %rax")
   with Match_failure(_) -> raise (Code_gen_failure_expression e)
@@ -113,13 +133,14 @@ let rec generate_asm_var = fun vl varl sp ->
 
 let rec generate_asm_statement ?retlbl = fun varl sp s il ->
   try match s with
+  | CompoundStmt( [], []) -> il
   | CompoundStmt([],sl) -> (try match sl with 
                           | [] -> il
                           | x::s -> let il2 = generate_asm_statement varl sp x il in
                             generate_asm_statement varl sp (CompoundStmt ([],s)) il2
                           with Match_failure(_) -> raise (Code_gen_failure_statment s)
                           )
-  | CompoundStmt(vl,sl) -> let (sp2,nvarl) = generate_asm_var vl varl sp in 
+  (*| CompoundStmt(vl,sl) -> let (sp2,nvarl) = generate_asm_var vl varl sp in 
                            let iln = il |+ "subq $"^string_of_int (sp2-sp)^", %rsp"in
                           (try match sl with 
                           | [] -> il
@@ -127,7 +148,15 @@ let rec generate_asm_statement ?retlbl = fun varl sp s il ->
                                     let il3 = generate_asm_statement nvarl sp2 (CompoundStmt ([],s)) il2 in
                                     (il3 |+ "addq $"^string_of_int (sp2-sp)^", %rsp")
                           with Match_failure(_) -> raise (Code_gen_failure_statment s)
-                          )
+                          )*)
+  | CompoundStmt( tv::qv, ls) -> 
+      let sp2 = sp+8 in
+      let nvarl = (tv,(string_of_int (-sp2))^"(%rbp)")::varl in
+      (il
+         |+ "subq $8, %rsp"
+         |> generate_asm_statement ?retlbl nvarl sp2 (CompoundStmt (qv,ls))
+         |+ "addq $8, %rsp"
+     )
   | IfStmt (e,s1,s2) ->  let fin_l = fresh_lbl "fin_if" in
                   let else_l = fresh_lbl "else_if" in 
                   let il2 = generate_asm_expression varl sp e il in
@@ -139,16 +168,16 @@ let rec generate_asm_statement ?retlbl = fun varl sp s il ->
                   let il6 =  generate_asm_statement varl sp s2 il5 in   
                   (il6 |+ (fin_l)^":") 
    |Expr e -> (generate_asm_expression varl sp e il)     
-   |WhileStmt (e,s) ->  let d_l = fresh_lbl "debut_while" in
-                        let f_l = fresh_lbl "fin_while" in 
-                        let ill = il |+ (d_l)^":" in
-                        let il2 = generate_asm_expression varl sp e ill in 
-                        let il3 = il2 |+ "testq %rax, %rax"
-                            |+ "jz "^f_l in       
-                        let il4 =  generate_asm_statement varl sp s il3 in
-                        let il5 = il4 |+ "jmp "^d_l 
-                            |+ (f_l)^":" in  
-                        il5
+   |WhileStmt(e,s) ->
+     let le = fresh_lbl "while_end"
+     and lb = fresh_lbl "while_begin" in
+     let il2 = il |+ lb ^":" in
+     let il3 = generate_asm_expression varl sp e il2
+         |+ "testq %rax, %rax"
+         |+ "jz "^le in
+     generate_asm_statement ?retlbl varl sp s il3
+         |+ "jmp "^lb
+         |+ le^":"
                                   
   | ReturnStmt None ->
      (il
